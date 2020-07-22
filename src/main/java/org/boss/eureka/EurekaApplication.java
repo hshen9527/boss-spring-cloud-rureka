c375f4d6438d63ee4d0b33be36fd4da3e33fbe5d
WLX <2391255979@qq.com>
2020-07-22 11:25:00 +0800

    重新打包

--- src/main/java/org/boss/eureka/EurekaApplication.java
+++ src/main/java/org/boss/eureka/EurekaApplication.java
@@ -8,7 +8,12 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaApplication {
+public class EurekaApplication extends SpringBootServletInitializer{
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(EurekaApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class, args);
